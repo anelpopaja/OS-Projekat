a783d0afb716bc80b45396594ef10e41320e3d58
Anel <anel.popaja5@gmail.com>
2021-10-23 17:28:16 +0200

    Implemented Scheduler

--- src/processes/ProcesScheduler.java
+++ src/processes/ProcesScheduler.java
@@ -1,5 +1,20 @@
 package processes;
 
+import memory.Memorija;
+
+import java.util.Queue;
+
 public class ProcesScheduler {
+    public static synchronized void schedule() {
+        Queue<Proces> readyQueue = Memorija.getReadyQueue();
+        if(!readyQueue.isEmpty() && Memorija.getRunning_proces() == null) {
+            Proces proces = readyQueue.remove();
+            Memorija.setRunning_process(proces);
+            proces.setState("RUNNING");
+            //TODO
+            //new ExecutionThread(process);
+
+        }
+    }
 
 }
